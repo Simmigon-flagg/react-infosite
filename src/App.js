e818db89aa218e9f0c44647aca21871c2d5b5212
Simmigon Flagg <Simmigon.Flagg@macys.com>
2022-04-13 12:41:33 -0400

    Single Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        React Starter
-      </header>
-    </div>
-  );
+import React from "react"
+import "./app.css" 
+import Header from "../src/components/Header"
+import Footer from "../src/components/Footer"
+import MainContent from "../src/components/MainContent"
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
 }
 
 export default App;
